ab062dcdedb67e23891291d735c7087c4f5c7d35
shahzamanbaloch <68694775+shahzamanbaloch@users.noreply.github.com>
2021-08-13 15:10:39 +0100

    Add files via upload

--- cypress/integration/examples/BDD/tommy/signUp.js
+++ cypress/integration/examples/BDD/tommy/signUp.js
@@ -2,8 +2,6 @@ const locator = require('../../../../locators/tommy_register.json');
 
 class signUp {
 
-    
-
     closeButton(){
     cy.get(locator.closeButton).click();
     return this
@@ -18,18 +16,15 @@ class signUp {
         return this
     }
 
-
-
     email() {
        
-            this.data = data;
-     cy.get(locator.emailInput).clear().type(this.data.email);
+     cy.get(locator.emailInput).clear().type('shaz123@test.com');
         return this
       
     }
 
     Password() {
-        cy.get(locator.passwordInput).clear().type(this.data.password);
+        cy.get(locator.passwordInput).clear().type('shaz123@1');
         return this
     }
     checkboxLabel(){
@@ -42,18 +37,15 @@ class signUp {
         cy.get(locator.primaryButton).click();
     }
 
-
-    // Invalid 
         invalidData(){
           
-                this.data = data;
             cy.get(locator.closeButton).click();
             cy.get(locator.signButton).click();
             cy.get(locator.registerButton).click();
             cy.get(locator.email).clear();
-            cy.get(locator.email).type(this.data.Invalidemail);
+            cy.get(locator.email).type('tommy@example');
             cy.get(locator.passwordInput).clear();
-            cy.get(locator.passwordInput).type(this.data.Invalidpassword);
+            cy.get(locator.passwordInput).type('shaz');
             cy.get(locator.checkboxLabel).click();
             cy.get(locator.checkbox).check();
             cy.get(locator.newsletterLabel).click();
@@ -63,50 +55,40 @@ class signUp {
             cy.get(locator.backdrop).click();
             cy.get(locator.backdrop).click();
             cy.get(locator.primaryButton).click();
-           
+
         }
         
-
-        duplicateUser(){
-            
-                this.data = data;
+        duplicateUser(){   
          cy.get(locator.closeButton).click();
         cy.get(locator.signButton).click();
         cy.get(locator.registerButton).click();
-        
         cy.get(locator.email).clear();
-        cy.get(locator.email).type(this.data.Duplicateemail);
+        cy.get(locator.email).type('tes1t@example.com');
         cy.get(locator.passwordInput).clear();
-        cy.get(locator.passwordInput).type(this.data.Duplicatepassword);
+        cy.get(locator.passwordInput).type('tomm1234567');
         cy.get(locator.checkboxLabel).click();
         cy.get(locator.newsletterLabel).click();
         cy.get(locator.primaryButton).click();
-        /* ==== Generated with Cypress Studio ==== */    //Oeps, er lijkt al een account met dit e-mailadres te bestaan.
         cy.get(locator.error).contains('Oeps, er lijkt al een account met dit e-mailadres te bestaan').should('exist')
   
     }
-
         withoutTermsandCondition(){
           
-                this.data = data;
             cy.get(locator.closeButton).click();
             cy.get(locator.signButton).click();
             cy.get(locator.registerButton).click();
-        
-            
             cy.get(locator.email).clear();
-            cy.get(locator.email).type(this.data.withoutemail);
+            cy.get(locator.email).type('shaz@exam');
             cy.get(locator.passwordInput).clear();
-            cy.get(locator.passwordInput).type(this.data.withoutpassword);
+            cy.get(locator.passwordInput).type('Test1234@');
             //cy.get('.agree-terms > [data-testid=checkbox-label]').click();
             cy.get(locator.newsletterLabel).click();
             cy.get(locator.primaryButton).click();
                 //Oeps, er lijkt al een account met dit e-mailadres te bestaan.
-            // cy.get('.error-text').should('exist')  //Bevestig dat je akkoord gaat met de algemene voorwaarden
+             cy.get(locator.error).should("include.text", "Bevestig dat je akkoord gaat met de algemene voorwaarden")
       
             cy.get(locator.backdrop).click();
             cy.get(locator.error).click();
-         
             cy.get(locator.modelClosebtn).click();
      
         }
